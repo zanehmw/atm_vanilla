90fe747b23b975ff7f64314e8ce3cd038ad18ac9
Tyler Crosse <tylerscottcrosse@gmail.com>
2016-03-23 17:42:57 -0400

    Add class toggle

--- atm.js
+++ atm.js
@@ -5,6 +5,11 @@ var atm = {
     checking: 0,
     savings: 0
   },
+  listen: function(){
+    for (var i = 0; i < this.buttons.length; i++) {
+      this.buttons[i].addEventListener('click', this.transact.bind(this))
+    }
+  },
   transact: function(event){
     var account = event.target.parentNode
     var accountType = account.querySelector("h2").innerHTML.toLowerCase()
@@ -14,18 +19,26 @@ var atm = {
     var button = event.target.value
     if( button == "Deposit"){
       // add user input to the relevant account
+      atm.changeClass(accountType);
       this.accounts[accountType] +=  userInput
-    } else if( button == "Withdraw" && this.accounts[accountType] >= userInput) {
+    } else if (button == "Withdraw" && this.accounts[accountType] > userInput) {
       this.accounts[accountType] -=  userInput
-    } else{
+    } else if (button == "Withdraw" && this.accounts[accountType] == userInput) {
+      this.accounts[accountType] -=  userInput
+      atm.changeClass(accountType);
+    } else {
       alert("You don't have enough money")
     }
     // update the html for that account
     account.querySelector(".balance").innerHTML = "$" + this.accounts[accountType]
   },
-  listen: function(){
-    for (var i = 0; i < this.buttons.length; i++) {
-      this.buttons[i].addEventListener('click', this.transact.bind(this))
+  changeClass: function(accountType){
+    var elName = "#"+ accountType +"BalanceDiv"
+    var classNames = document.querySelector(elName).classList
+    if (classNames.contains("zero")) {
+      classNames.remove("zero")
+    } else {
+      classNames.add("zero")
     }
   }
 }
